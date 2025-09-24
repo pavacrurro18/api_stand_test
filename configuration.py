c9630bf1015ba1903cd326c0cfa652236fc6d3e0
Paula Cruz <134227467+pavacrurro18@users.noreply.github.com>
2025-09-24 13:57:45 -0500

    configuration.py

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-558a8a80-352e-49f8-910b-43e98a68e107.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/" #Esta ruta es la que crea un usuario
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
